8c9f8136f8331242f794c0c754c14c936187ae88
John Altidor <jgaltidor@gmail.com>
2020-02-18 20:50:20 -0600

    minor update: fixed spacing

--- new_examples/standalones/mac_flatten/student/Student.cpp
+++ new_examples/standalones/mac_flatten/student/Student.cpp
@@ -7,9 +7,9 @@ Student::Student(const char* para0,jint para1,jfloat para2) throw ()
 {
 	JNIEnv* env;
 	JavaVM* jvm = JNU_GetJavaVM(&env);
-	jclass clazz=env->FindClass("Student");
-	jmethodID methodID=env->GetMethodID(clazz,"<init>","(Ljava/lang/String;IF)V");
-	javaObject=env->NewObject(clazz,methodID, env->NewStringUTF(para0), para1, para2);
+	jclass clazz = env->FindClass("Student");
+	jmethodID methodID = env->GetMethodID(clazz,"<init>","(Ljava/lang/String;IF)V");
+	javaObject = env->NewObject(clazz,methodID, env->NewStringUTF(para0), para1, para2);
 
 	JNU_handleJavaException(env);
 
@@ -19,8 +19,8 @@ char* Student::getName() throw ()
 {
 	JNIEnv* env;
 	JavaVM* jvm = JNU_GetJavaVM(&env);
-	jclass clazz=env->FindClass("Student");
-	jmethodID methodID=env->GetMethodID(clazz,"getName","()Ljava/lang/String;");
+	jclass clazz = env->FindClass("Student");
+	jmethodID methodID = env->GetMethodID(clazz,"getName","()Ljava/lang/String;");
 	char* returnThing = JNU_CreateCharsOfJString(env, (jstring) env->CallObjectMethod(javaObject,methodID));
 
 	JNU_handleJavaException(env);
@@ -32,8 +32,8 @@ jint Student::getAge() throw ()
 {
 	JNIEnv* env;
 	JavaVM* jvm = JNU_GetJavaVM(&env);
-	jclass clazz=env->FindClass("Student");
-	jmethodID methodID=env->GetMethodID(clazz,"getAge","()I");
+	jclass clazz = env->FindClass("Student");
+	jmethodID methodID = env->GetMethodID(clazz,"getAge","()I");
 	jint returnThing = env->CallIntMethod(javaObject,methodID);
 
 	JNU_handleJavaException(env);
@@ -45,8 +45,8 @@ jfloat Student::getGPA() throw ()
 {
 	JNIEnv* env;
 	JavaVM* jvm = JNU_GetJavaVM(&env);
-	jclass clazz=env->FindClass("Student");
-	jmethodID methodID=env->GetMethodID(clazz,"getGPA","()F");
+	jclass clazz = env->FindClass("Student");
+	jmethodID methodID = env->GetMethodID(clazz,"getGPA","()F");
 	jfloat returnThing = env->CallFloatMethod(javaObject,methodID);
 
 	JNU_handleJavaException(env);
@@ -58,8 +58,8 @@ void Student::setName(char* para0) throw ()
 {
 	JNIEnv* env;
 	JavaVM* jvm = JNU_GetJavaVM(&env);
-	jclass clazz=env->FindClass("Student");
-	jmethodID methodID=env->GetMethodID(clazz,"setName","(Ljava/lang/String;)V");
+	jclass clazz = env->FindClass("Student");
+	jmethodID methodID = env->GetMethodID(clazz,"setName","(Ljava/lang/String;)V");
 	env->CallVoidMethod(javaObject,methodID, env->NewStringUTF(para0));
 
 	JNU_handleJavaException(env);
@@ -69,8 +69,8 @@ void Student::setAge(jint para0) throw ()
 {
 	JNIEnv* env;
 	JavaVM* jvm = JNU_GetJavaVM(&env);
-	jclass clazz=env->FindClass("Student");
-	jmethodID methodID=env->GetMethodID(clazz,"setAge","(I)V");
+	jclass clazz = env->FindClass("Student");
+	jmethodID methodID = env->GetMethodID(clazz,"setAge","(I)V");
 	env->CallVoidMethod(javaObject,methodID, para0);
 
 	JNU_handleJavaException(env);
@@ -80,8 +80,8 @@ void Student::setGPA(jfloat para0) throw ()
 {
 	JNIEnv* env;
 	JavaVM* jvm = JNU_GetJavaVM(&env);
-	jclass clazz=env->FindClass("Student");
-	jmethodID methodID=env->GetMethodID(clazz,"setGPA","(F)V");
+	jclass clazz = env->FindClass("Student");
+	jmethodID methodID = env->GetMethodID(clazz,"setGPA","(F)V");
 	env->CallVoidMethod(javaObject,methodID, para0);
 
 	JNU_handleJavaException(env);
